1522b7fee4575aea24f1db6d6a10825b79aa936f
Nathan Skouby <ndskouby@gmail.com>
2024-02-10 20:30:33 -0600

    controller tests passing

--- app/controllers/training_activities_controller.rb
+++ app/controllers/training_activities_controller.rb
@@ -1,52 +1,51 @@
 class TrainingActivitiesController < ApplicationController
-    before_action :set_training_activity, only: [:show, :edit, :update, :destroy]
+  before_action :set_training_activity, only: [:show, :edit, :update, :destroy]
   
-    def index
-      @training_activities = TrainingActivity.all
+  def index
+    @training_activities = TrainingActivity.all
+  end
+  
+  def show
+  end
+
+  def new
+    @training_activity = TrainingActivity.new
+  end
+
+  def create
+    @training_activity = TrainingActivity.new(training_activity_params)
+    #@training_activity.status = 'Pending'
+
+    if @training_activity.save
+      redirect_to @training_activity, notice: 'Training activity was successfully created.'
+    else
+      render :new
     end
-  
-    def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @training_activity.update(training_activity_params)
+      redirect_to @training_activity, notice: 'Training activity was successfully updated.'
+    else
+      render :edit
     end
-  
-    def new
-      @training_activity = TrainingActivity.new
-    end
-  
-    def create
-      @training_activity = TrainingActivity.new(training_activity_params)
-      @training_activity.status = 'Pending'
-  
-      if @training_activity.save
-        redirect_to @training_activity, notice: 'Training activity was successfully created.'
-      else
-        render :new
-      end
-    end
-  
-    def edit
-    end
-  
-    def update
-      if @training_activity.update(training_activity_params)
-        redirect_to @training_activity, notice: 'Training activity was successfully updated.'
-      else
-        render :edit
-      end
-    end
-  
-    def destroy
+  end
+
+  def destroy
+    @training_activity = TrainingActivity.find(params[:id])
+    @training_activity.destroy
+    redirect_to training_activities_url, notice: 'Training activity was successfully destroyed.'
+  end
+
+  private
+    def set_training_activity
       @training_activity = TrainingActivity.find(params[:id])
-      @training_activity.destroy
-      redirect_to training_activities_url, notice: 'Training activity was successfully destroyed.'
     end
-  
-    private
-      def set_training_activity
-        @training_activity = TrainingActivity.find(params[:id])
-      end
-  
-      def training_activity_params
-        params.require(:training_activity).permit(:title, :activity_type, :description, :start_time, :end_time, :status)
-      end
+
+    def training_activity_params
+      params.require(:training_activity).permit(:title, :activity_type, :description, :start_time, :end_time, :status)
+    end
 end
-  
