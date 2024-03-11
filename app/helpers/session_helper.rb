b23ead803a9fe77252bc30a39efb568b8d612e02
Evan <evanpk@yahoo.com>
2024-03-10 22:47:50 -0500

    fix minor unit in session helper

--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -10,7 +10,7 @@ module SessionHelper
 
       # Making the default Minor Unit point to a Dummy entry
       # for the time being, since the audit system isn't up yet.
-      u.minor_unit_id = '1'
+      u.unit = Unit.find_by(name: "Unassigned Unit")
     end
   end
 end
