9193b3946575f918c22523335bfb488a5dff47d1
Evan <evanpk@yahoo.com>
2024-03-30 17:39:42 -0500

    use auto import

--- lib/ingest_roster_file.rb
+++ lib/ingest_roster_file.rb
@@ -3,70 +3,72 @@
 require 'csv'
 
 class IngestRosterFile
+  def confirm_unit(name, cat, parent)
+    return nil if name == ''
+  
+    unit = Unit.find_by(name:, cat:)
+    unit = Unit.create!(name:, cat:, parent:) if unit.nil?
+  
+    if unit.parent.nil?
+      unit.parent = parent
+      unit.save!
+    end
+  
+    if !parent.nil? && parent != unit.parent
+      puts "Skipping on #{name} #{cat} - wanting #{parent.name}, current #{unit.parent.name}"
+      return nil
+    end
+  
+    unit
+  end
+  
+  def create_units(row)
+    outfit_only =
+      row['Cadet/Major Unit'] == 'CORPS Staff' ||
+      row['Cadet/Major Unit'] == 'CS' ||
+      row['Cadet/Unit'].include?('Staff') ||
+      row['Cadet/Unit'] == 'LOA'
+  
+    if outfit_only
+      confirm_unit(row['Cadet/Outfit'], 'outfit', nil)
+    else
+      major = confirm_unit(row['Cadet/Major Unit'], 'major', nil)
+      minor = confirm_unit(row['Cadet/Minor Unit'], 'minor', major)
+      confirm_unit(row['Cadet/Outfit'], 'outfit', minor)
+    end
+  end
+  
+  def process_roster_row(row)
+    if row['Cadet/Outfit'].nil?
+      puts "Skipping #{row['Cadet/Email']}"
+      return
+    end
+  
+    create_units(row)
+  
+    User.create!(
+      email: row['Cadet/Email'],
+      first_name: row['Cadet/First'],
+      last_name: row['Cadet/Last'],
+      uid: nil,
+      provider: 'google_oauth2',
+      unit: Unit.find_by!(name: row['Cadet/Outfit'], cat: 'outfit')
+    )
+  end
+  
+  def ingest_roster_file(file_path)
+    csv = CSV.open(file_path, headers: true)
+    csv.each do |row|
+      process_roster_row(row)
+    end
+  end
 end
 
-def confirm_unit(name, cat, parent)
-  return nil if name == ''
 
-  unit = Unit.find_by(name:, cat:)
-  unit = Unit.create!(name:, cat:, parent:) if unit.nil?
-
-  if unit.parent.nil?
-    unit.parent = parent
-    unit.save!
-  end
-
-  if !parent.nil? && parent != unit.parent
-    puts "Skipping on #{name} #{cat} - wanting #{parent.name}, current #{unit.parent.name}"
-    return nil
-  end
-
-  unit
-end
-
-def create_units(row)
-  outfit_only =
-    row['Cadet/Major Unit'] == 'CORPS Staff' ||
-    row['Cadet/Major Unit'] == 'CS' ||
-    row['Cadet/Unit'].include?('Staff') ||
-    row['Cadet/Unit'] == 'LOA'
-
-  if outfit_only
-    confirm_unit(row['Cadet/Outfit'], 'outfit', nil)
-  else
-    major = confirm_unit(row['Cadet/Major Unit'], 'major', nil)
-    minor = confirm_unit(row['Cadet/Minor Unit'], 'minor', major)
-    confirm_unit(row['Cadet/Outfit'], 'outfit', minor)
-  end
-end
-
-def process_roster_row(row)
-  if row['Cadet/Outfit'].nil?
-    puts "Skipping #{row['Cadet/Email']}"
-    return
-  end
-
-  create_units(row)
-
-  User.create!(
-    email: row['Cadet/Email'],
-    first_name: row['Cadet/First'],
-    last_name: row['Cadet/Last'],
-    uid: nil,
-    provider: 'google_oauth2',
-    unit: Unit.find_by!(name: row['Cadet/Outfit'], cat: 'outfit')
-  )
-end
-
-def ingest_roster_file(file_path)
-  csv = CSV.open(file_path, headers: true)
-  csv.each do |row|
-    process_roster_row(row)
-  end
-end
 
 if __FILE__ == $PROGRAM_NAME
   base_path = 'lib/assets/Overhead - Master Cadet Roster.csv'
   base_path = Regexp.last_match(1) if Regexp.last_match(1)
-  ingest_roster_file(base_path)
+
+  IngestRosterFile.new.ingest_roster_file(base_path)
 end
--- spec/lib/ingest_roster_file_spec.rb
+++ spec/lib/ingest_roster_file_spec.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-require_relative '../../lib/ingest_roster_file'
 
 RSpec.feature 'Roster Ingestor' do
   describe 'Load File' do
     it 'loads units correctly' do
-      ingest_roster_file('spec/support/fixtures/demoCorpsRoster.csv')
+      IngestRosterFile.new.ingest_roster_file('spec/support/fixtures/demoCorpsRoster.csv')
       user = User.find_by!(email: 'e@a.com')
       expect(user.unit.name).to eq('A1')
       expect(user.unit.parent.name).to eq('3BN')
