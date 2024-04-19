b8fa9c14aa1fff62ffc522c19f951415b64bf4e9
Alan Chao <alanchao8669@tamu.edu>
2024-04-18 23:46:10 -0500

    Fix rspec test

--- spec/lib/ingest_roster_file_spec.rb
+++ spec/lib/ingest_roster_file_spec.rb
@@ -6,11 +6,11 @@ RSpec.feature 'Roster Ingestor' do
   describe 'Load File' do
     it 'loads units correctly' do
       IngestRosterFile.new.ingest_roster_file('spec/support/fixtures/demoCorpsRoster.csv')
-      user = User.find_by!(email: 'e@a.com')
-      expect(user.unit.name).to eq('A1')
-      expect(user.unit.parent.name).to eq('3BN')
+      user = User.find_by!(email: 'c@a.com')
+      expect(user.unit.name).to eq('K1')
+      expect(user.unit.parent.name).to eq('2BN')
       expect(user.unit.parent.parent.name).to eq('1BDE')
-      expect(user.units).to match_array([Unit.find_by(name: 'A1'), Unit.find_by(name: '3BN'), Unit.find_by(name: '1BDE')])
+      expect(user.units).to match_array([Unit.find_by(name: 'K1'), Unit.find_by(name: '2BN'), Unit.find_by(name: '1BDE'), Unit.find_by(name: 'CMDT Staff')])
     end
   end
 end
