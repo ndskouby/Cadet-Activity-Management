9618e2d14e17e013ba0982146b4f70226983e00e
Manas Sahu <manas.sahu.212@gmail.com>
2024-04-20 05:55:53 -0500

    Edit User data works now

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,50 +1,54 @@
-require 'ingest_roster_file'
-
 class AdminController < ApplicationController
     before_action :set_user, only: %i[show edit update destroy]
+  
     def index
-        @users = User.all
+      @users = User.all
     end
-
+  
     def show
     end
-
-    def new 
-        @user = User.new
+  
+    def new
+      @user = User.new
     end
-
-    def destroy 
-        @user.destroy
-        redirect_to admin_url, notice: 'User was sucessfully deleted'
+  
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        redirect_to admin_index_url, notice: 'User was successfully created'
+      else
+        render :new
+      end
     end
-
-    def update 
-        if @user.update(user_params)
-            redirect_to @user, notice: "User was successfully updated"
-        else 
-            render :edit
-        end 
-    end
-
+  
     def edit
     end
-
-    def create
-        @user = User.new(user_params)
-        respond_to do |format|
-            if @user.save
-              handle_successful_save(format)
-            else
-              handle_failed_save(format)
-            end
-        end
+  
+    def update
+      if @user.update(user_params)
+        redirect_to admin_index_url, notice: 'User was successfully updated'
+      else
+        render :edit
+      end
+    end
+  
+    def destroy
+      @user.destroy
+      redirect_to admin_index_url, notice: 'User was successfully deleted'
+    end
+    
+    def import 
+        
     end
 
-    def set_user 
-        @user = User.find(params[:id])
+    private
+  
+    def set_user
+      @user = User.find(params[:id])
     end
-
+  
     def user_params
-        params.require(:user).permit(:first_name, :last_name, :email, :unit_id)
+      params.require(:user).permit(:first_name, :last_name, :email, :unit_id)
     end
-end
+  end
+  
